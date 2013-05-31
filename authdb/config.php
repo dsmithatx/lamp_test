44a2585f83a0196e6d5942b1a01283473f8d98d0
Doug Smith <dsmith@datapipe.com>
2013-05-31 18:34:01 +0000

    first commit

---
+++ authdb/config.php
@@ -0,0 +1,9 @@
+<?php
+// db properties
+// This file managed by puppet for dsmith-node
+// Manual changes will be reverted.
+$dbhost = 'localhost';
+$dbname = 'lampsitetest';
+$dbpass = 'lampsitetest123';
+$dbuser = 'lampsiteuser';
+?>
